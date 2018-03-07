4a8bb5ebf0b1764bd4748a410864227fb536ec05
alznn <zxcv19960410@gmail.com>
2018-03-07 11:51:03 +0800

    前處理成yaml

--- xlsxTest.py
+++ xlsxTest.py
@@ -2,6 +2,19 @@ import xlrd
 from itertools import takewhile
 import xlrd
 
+import yaml
+import re
+
+number = "[0-9]+"
+data = []
+#============================以下是 yaml======================================
+def regEXP(string):
+    if re.findall(number, string):
+        for reg in re.findall(number, string):
+            tmp = str(reg)
+            string = string.replace(tmp, "'"+tmp+"'")
+    return string
+#============================以上是 yaml======================================
 def column_len(sheet, index):
     col_values = sheet.col_values(index)
     col_len = len(col_values)
@@ -22,3 +35,18 @@ for col in range(0,63): #check sheet 1
         if( sheet.cell(rowx = index, colx = col).value=="" ):
             continue
         print ( sheet.cell(rowx = index, colx = col).value)
+
+#==============================以下是 yaml ====================================            
+with open('氣象詞彙與編碼表.txt','r',encoding='utf8') as fp:
+    for line in fp:
+        line = regEXP(line)
+        data.append(line)
+fp.close()
+
+F = open('氣象詞彙與編碼表.yaml',"w",encoding="utf8")  
+print(len(data))
+for i in range(0, len(data)):
+    print (data[i])
+    F.write(data[i])
+F.close()
+#============================以上是 yaml======================================
